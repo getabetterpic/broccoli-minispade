989fcc70b7933ed9cece5fde902b10a6f20b92a6
Daniel Smith <dsmith@primerevenue.com>
2015-11-04 13:28:55 -0500

    Add rewriteRequire option

--- index.js
+++ index.js
@@ -12,12 +12,17 @@ function MinispadeFilter(inputTree, options) {
   this.inputTree = inputTree;
   this.extensions = [ 'js' ];
   this.useSourceUrl = options.useSourceUrl;
+  this.rewriteRequire = options.rewriteRequire;
 }
 
 MinispadeFilter.prototype.targetExtension = 'js';
 
 MinispadeFilter.prototype.processString = function(code, name) {
   var contents = '';
+  if (this.rewriteRequire) {
+    code.replace(/^\s*require\s*\(\s*/, 'minispade.require(');
+    code.replace(/^\s*requireAll\s*\(\s*/, 'minispade.requireAll(');
+  }
   var moduleId = name.replace('lib/', '').replace('.js', '');
   if (this.useSourceUrl === true) {
     contents = JSON.stringify("(function() {" + code + "})();//# sourceURL=" + moduleId);
