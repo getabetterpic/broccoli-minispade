cf9b63af5e53b5f2bb6161f18465310243f83af4
Daniel Smith <dsmith@primerevenue.com>
2015-11-06 17:52:01 -0500

    Change replace to take path into account

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Filter = require('broccoli-filter');
+var path = require('path');
 
 module.exports = MinispadeFilter;
 
@@ -18,16 +19,19 @@ function MinispadeFilter(inputTree, options) {
 MinispadeFilter.prototype.targetExtension = 'js';
 
 MinispadeFilter.prototype.processString = function(code, name) {
-  var contents = '';
+  var contents = '', this_dir = path.dirname(name);
+  if (this.rewriteRequire) {
+    contents = contents.replace(/\s*(require|requireAll)\s*\(\s*[\'\"]([^\'\"]*)[\'\"]\s*\)\s*/g, function(match, p1, p2) {
+      path = p2[0] === '.' ? p2.slice(1) : p2;
+      path = this_dir + path;
+      return "minispade." + p1 + "('" + path + "')";
+    });
+  }
   var moduleId = name.replace(/(lib\/|\/index)/, '').replace('.js', '');
   if (this.useSourceUrl === true) {
     contents = JSON.stringify("(function() {" + code + "})();//# sourceURL=" + moduleId);
   } else {
     contents = "function() {" + code + "}";
-  }
-  if (this.rewriteRequire) {
-    contents = contents.replace(/\s*require\s*\(\s*/g, 'minispade.require(');
-    contents = contents.replace(/\s*requireAll\s*\(\s*/g, 'minispade.requireAll(');
   }
   return "minispade.register('" + moduleId + "'," + contents + ");";
 }
