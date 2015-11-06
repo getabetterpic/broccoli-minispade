d09154d1e29fba05134533db6e68d3338241e847
Daniel Smith <dsmith@primerevenue.com>
2015-11-06 17:55:56 -0500

    Move the replace function to the correct spot

--- index.js
+++ index.js
@@ -20,18 +20,18 @@ MinispadeFilter.prototype.targetExtension = 'js';
 
 MinispadeFilter.prototype.processString = function(code, name) {
   var contents = '', this_dir = path.dirname(name);
+  var moduleId = name.replace(/(lib\/|\/index)/, '').replace('.js', '');
+  if (this.useSourceUrl === true) {
+    contents = JSON.stringify("(function() {" + code + "})();//# sourceURL=" + moduleId);
+  } else {
+    contents = "function() {" + code + "}";
+  }
   if (this.rewriteRequire) {
     contents = contents.replace(/\s*(require|requireAll)\s*\(\s*[\'\"]([^\'\"]*)[\'\"]\s*\)\s*/g, function(match, p1, p2) {
       path = p2[0] === '.' ? p2.slice(1) : p2;
       path = this_dir + path;
       return "minispade." + p1 + "('" + path + "')";
     });
-  }
-  var moduleId = name.replace(/(lib\/|\/index)/, '').replace('.js', '');
-  if (this.useSourceUrl === true) {
-    contents = JSON.stringify("(function() {" + code + "})();//# sourceURL=" + moduleId);
-  } else {
-    contents = "function() {" + code + "}";
   }
   return "minispade.register('" + moduleId + "'," + contents + ");";
 }
